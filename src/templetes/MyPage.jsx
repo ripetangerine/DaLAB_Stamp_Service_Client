862804d751f3e26e704bdf0e7916510f9932e735
leehyunmin <leehm2292@gmail.com>
2025-11-13 15:10:01 +0900

    add: navgiatorbar 컴포넌트화

--- src/templetes/MyPage.jsx
+++ src/templetes/MyPage.jsx
@@ -1,5 +1,6 @@
 import react from 'react';
 import { useState, useEffect } from 'react';
+import NavigatorBar from '../components/NavigatorBar';
 
 export default function MyPage() {
 
@@ -7,16 +8,7 @@ export default function MyPage() {
         <>
             <div className="bg-linear-180 from-white to-[#F2F7FF] w-screen min-h-screen">
 
-                <div className='w-full min-h-15 shadow-md shadow-gray-100 flex items-center justify-between px-6 py-3'>
-                    <div className='flex items-center'>
-                        <div className="bg-[url('/assets/images/mainlogo.png')] w-12 h-8 bg-cover mr-3"></div>
-                        <div className='font-semibold text-1.5xl'>Stamp Marble</div>
-                    </div>
-                    <div className='flex items-center'>
-                        <div className="bg-[url('/assets/images/alarm.png')] w-6.5 h-7 bg-cover bg-no-repeat mr-8"></div>
-                        <div className="bg-[url('/assets/images/user.png')] w-7 h-7 bg-cover bg-no-repeat"></div>
-                    </div>
-                </div>
+                <NavigatorBar />
 
                 <div className='flex flex-col w-screen items-center'>
                     <a href='' className="w-6 h-9 bg-[url('/assets/images/back.png')] bg-cover mt-10 ml-25 self-start"></a>
@@ -30,8 +22,9 @@ export default function MyPage() {
                             </div>
                         </div>
 
-                        <div className='text-white bg-background p-1 rounded-md cursor-pointer mr-55 mt-5'>
-                            정보 수정하기
+                        <div className='text-white bg-background pt-1 pb-1 pl-2 pr-2 rounded-md cursor-pointer mr-55 mt-5 flex justify-center items-center'>
+                            <div className='mr-2'>정보 수정하기</div>
+                            <div className="bg-[url('/assets/images/Vector.png')] w-4 h-4 bg-no-repeat bg-cover"></div>
                         </div>
                     </div>
                     <hr className='w-5xl mb-5' />
